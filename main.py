84eb69b06f8b72c8a287d5cc747ede98dbb3a66f
Databank Academy <databank_mlops@gmail.com>
2025-03-31 13:10:51 -0500

    feat(main.py): Se añade nueva funcionalidad en el print

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from databank_mlops.logger.logger import logger
 for i in range(10000000):
     logger.debug(f"Versión del package: {databank_mlops.__version__}")
     logger.info(f"Versión del package: {databank_mlops.__version__}")
-    logger.info(f"Versión del package: {databank_mlops.__version__}")
+    logger.info(f"Versión del package: {databank_mlops.__version__}" + "#############################")
     logger.error(f"Versión del package: {databank_mlops.__version__}")
